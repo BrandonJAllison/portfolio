082fcffdd2e1a0e9af5c2d0ef0396a477bb0390c
Brandon Allison <brandon.james.allison@gmail.com>
2020-03-23 10:55:32 -0400

    social icons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Components/Header.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJs, faHtml5, faCss3Alt, faDocker, faNode, faReact} from '@fortawesome/free-brands-svg-icons'
+import { faJs, faHtml5, faCss3Alt, faDocker, faNode, faReact, faTwitter, faLinkedinIn, faGithubAlt} from '@fortawesome/free-brands-svg-icons'
 
 import './App.css';
 
@@ -29,6 +29,11 @@ function App() {
 
   <header class="showcase">
     <div class="container showcase-inner">
+      <div>
+      <FontAwesomeIcon className="social-icon" icon={faTwitter}/>
+      <FontAwesomeIcon className="social-icon" icon={faLinkedinIn}/>
+      <FontAwesomeIcon className="social-icon" icon={faGithubAlt} />
+      </div>
       <h1>Welcome!</h1>
       <h3>I am Brandon Allison,  a software developer located in Pennsylvania.</h3>
       <div>
