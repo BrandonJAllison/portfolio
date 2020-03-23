3d20f11984cc86e6497237977504e80b46d60c2d
Brandon Allison <brandon.james.allison@gmail.com>
2020-03-23 10:09:38 -0400

    new landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import Header from './Components/Header.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJs, faHtml5, faCss3Alt, faDocker} from '@fortawesome/free-brands-svg-icons'
+import { faJs, faHtml5, faCss3Alt, faDocker, faNode, faReact} from '@fortawesome/free-brands-svg-icons'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <div style={{display:'flex', flexDirection:'column', alignItems:'center', height: '80vh', justifyContent:'center'}}>
+   
+<div class="menu-wrap">
+    <input type="checkbox" class="toggler"/>
+    <div class="hamburger"><div></div></div>
+    <div class="menu">
       <div>
-      <h2>Brandon J. Allison</h2>
-      <h3>Portfolio Coming Soon ...</h3>
+        <div>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
       </div>
+    </div>
+  </div>
+  
+
+  <header class="showcase">
+    <div class="container showcase-inner">
+      <h1>Hello!</h1>
+      <p>My name is Brandon Allison.  I am a web developer located in Pennsylvania.</p>
       <div>
-      <FontAwesomeIcon className="icon" icon={faJs} />
       <FontAwesomeIcon className="icon" icon={faHtml5} />
       <FontAwesomeIcon className="icon" icon={faCss3Alt}/>
+      <FontAwesomeIcon className="icon" icon={faJs} />
+      <FontAwesomeIcon className="icon" icon={faReact} />
+      <FontAwesomeIcon className="icon" icon={faNode} />
       <FontAwesomeIcon className="icon" icon={faDocker} />
       </div>
-      </div>
+    </div>
+  </header>
 
     </div>
   );
